60e6deccf8b31f98997827ee9a9864840100ce18
ajayi-george <128624260+ajayi-george@users.noreply.github.com>
2023-06-14 02:54:20 +0100

    Update email_check.py

--- email_check.py
+++ email_check.py
@@ -2,7 +2,7 @@ import requests
 
 
 def validate(email):
-    api_key = 'YtCeYFU2.qaVsq1BBzUZjW9oDfk2scCFlC6crOXKw' # Generated in your User Profile it shows at the top in a green bar once
+    api_key = 'Y' # Generated in your User Profile it shows at the top in a green bar once
     team_slug = "georgeajakadrex" # when you sign up you have a team, its in the URL then use that
     email_address = email # the test email
 
